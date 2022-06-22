e560226abce1f8d829f073ad59b775ca9013e743
Javi0300 <93288606+Javi0300@users.noreply.github.com>
2022-06-21 19:48:07 -0500

    Add files via upload

---
+++ assets/nodosNavegacion/boton.js
@@ -0,0 +1,30 @@
+// Scroll up
+
+document.getElementById("botones").addEventListener("click", scrollUp);
+
+function scrollUp(){
+
+    var currentScroll = document.documentElement.scrollTop;
+
+    if (currentScroll > 0){
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo (0, currentScroll - (currentScroll / 10));
+    }
+}
+
+
+///
+
+buttonUp = document.getElementById("botones");
+
+window.onscroll = function(){
+
+    var scroll = document.documentElement.scrollTop;
+
+    if (scroll > 500){
+        buttonUp.style.transform = "scale(1)";
+    }else if(scroll < 500){
+        buttonUp.style.transform = "scale(0)";
+    }
+
+}
